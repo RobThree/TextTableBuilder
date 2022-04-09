49e2986c781ebfb687e5dea01d7047be65684e67
RobThree <rob@devcorner.nl>
2022-04-09 12:55:48 +0200

    Simplified Table object and AddRow/AddColumn methods.

---
+++ TextTableBuilder.Tests/TableTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TextTableBuilder.Tests;
+
+[TestClass]
+public class TableTests
+{
+    [TestMethod]
+    public void Adding_Rows_Without_Columns_Throws()
+    {
+    }
+
+}
