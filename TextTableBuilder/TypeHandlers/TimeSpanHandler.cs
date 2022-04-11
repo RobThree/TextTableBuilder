5c4bc356df4de2dce365288e21365089d3a360bd
RobIII <r.janssen@keensystems.eu>
2022-04-11 18:37:49 +0200

    Fix timespan

--- TextTableBuilder/TypeHandlers/TimeSpanHandler.cs
+++ TextTableBuilder/TypeHandlers/TimeSpanHandler.cs
@@ -2,7 +2,7 @@
 
 public class TimeSpanHandler : SpecificFormatHandler
 {
-    public TimeSpanHandler(string format = "HH:mm:ss")
+    public TimeSpanHandler(string format = @"hh\:mm\:ss")
         : base(format) { }
 
     public override string Handle(object value, IFormatProvider formatProvider)
